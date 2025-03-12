da1c4f4e324a497475533fdeaaf10bd54e69555a
jarickperreault <jarick.perreault09@hotmail.com>
2025-03-12 09:46:20 -0400

    Ajout fonctions "déconnecter" et "query" dans Database.php

--- models/Database.php
+++ models/Database.php
@@ -1,4 +1,6 @@
-<?php 
+<?php
+# Lien pour les refs des functions MySQLi : https://www.w3schools.com/php/php_ref_mysqli.asp
+
 #Fonction pour se connecter à la BD MySQL
 function connect(){
     $connection = mysqli_connect(CONFIGURATIONS['servername'], CONFIGURATIONS['username'], CONFIGURATIONS['password'], CONFIGURATIONS['dbname']);
@@ -8,5 +10,23 @@ function connect(){
     }
 
     echo "Connection reussi!";
+
+    return $connection;
 }
+#Function pour se déconnecter de la BD
+function disconnect($connection){
+    if ($connection) {
+        mysqli_close($connection);
+        echo "Déconnection reussi!";
+    }
+}
+#Fonction pour exécuter une query SQL
+function query($query){
+    $connection = connect();
+    if ($result = mysqli_query($connection, $query)) {
+        return $result;
+    }
+    disconnect($connection);
+}
+
 ?>
