1d45df2504229cfbe4102899a42a95b06807cab0
jarickperreault <jarick.perreault09@hotmail.com>
2025-03-13 13:02:41 -0400

    Déplacé database et remplacement de MySQLi par PDO

--- src/database.php
+++ src/database.php
@@ -2,27 +2,24 @@
 # Lien pour les refs des functions MySQLi : https://www.w3schools.com/php/php_ref_mysqli.asp
 
 #Fonction pour se connecter à la BD MySQL
-function connect(){
-    $connection = mysqli_connect(CONFIGURATIONS['servername'], CONFIGURATIONS['username'], CONFIGURATIONS['password'], CONFIGURATIONS['dbname']);
-
-    if (!$connection) {
-        exit("Connection échoué: " . mysqli_connect_error());
+function getPdo(){
+    try{
+        return new PDO("mysql:host=".CONFIGURATIONS["servername"].";dbname=".CONFIGURATIONS["dbname"], CONFIGURATIONS["username"], CONFIGURATIONS["password"], DB_PARAMS);
     }
-
-    echo "Connection reussi!";
-
-    return $connection;
+    catch(PDOException $erreur){
+        throw new PDOException($erreur->getMessage());
+    }
 }
 #Function pour se déconnecter de la BD
 function disconnect($connection){
     if ($connection) {
-        mysqli_close($connection);
+        $connection = null;;
         echo "Déconnection reussi!";
     }
 }
 #Fonction pour exécuter une query SQL
-function query($query){
-    $connection = connect();
+function insert($query){
+    $connection = getPdo();
     if ($result = mysqli_query($connection, $query)) {
         return $result;
     }
