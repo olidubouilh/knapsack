792df0ee9c2b20ac6967450a6ccd7a775c4d7718
Rafye <raphleduc17@gmail.com>
2025-03-31 09:53:21 -0400

    Ajout de commentaire pour les procédures et les fonctions des boutons

--- public/javascript/buttons.js
+++ public/javascript/buttons.js
@@ -1,3 +1,12 @@
+/**
+ * Augmente la quantité affichée d'un élément spécifique de 1, 
+ * jusqu'à un maximum spécifié.
+ *
+ * @param {string} id - L'ID de l'élément dont la quantité va être augmentée.
+ * @param {number} maxQty - La quantité maximale autorisée pour l'élément.
+ * @return {void}
+ */
+
 function increaseQuantity(id, maxQty){
     const Element = document.getElementById(id).textContent;
     if(parseInt(Element) < maxQty){
@@ -9,6 +18,13 @@ function increaseQuantity(id, maxQty){
     document.getElementById(id).textContent = quantity;
 }
 
+/**
+ * Réduit la quantité affichée d'un item spécifique de 1, en s'assurant que la quantité ne soit pas en dessous de 1.
+ *
+ * @param {string} id - L'ID de l'élément duquel la quantité va être réduite.
+ * @return {void}
+ */
+
 function decreaseQuantity(id){
     const Element = document.getElementById(id).textContent;
     if(parseInt(Element) > 1){
@@ -19,19 +35,15 @@ function decreaseQuantity(id){
     }
     document.getElementById(id).textContent = quantity;
 }
-function payerPanier(id, montantTotal){
 
-}
-document.addEventListener('DOMContentLoaded', () => {
-    updateTotal();
 
-    document.querySelectorAll('.boutonquanti').forEach(button => {
-        button.addEventListener('click', () => {
-            setTimeout(updateTotal, 50); 
-        });
-    });
-});
 
+/**
+ * Met à jour le prix total affiché en fonction des quantités 
+ * définies pour chaque article dans le panier.
+ *
+ * @return {void}
+ */
 function updateTotal() {
     let total = 0;
 
@@ -47,6 +59,17 @@ function updateTotal() {
 
 
 
+/**
+ * Supprime un article du panier après confirmation de l'utilisateur.
+ * 
+ * Cette fonction envoie une requête POST au serveur pour supprimer un article
+ * du panier en utilisant son identifiant et celui de l'utilisateur. Si la
+ * suppression est réussie, l'article est retiré de l'affichage et un message
+ * de confirmation est affiché. En cas d'erreur, un message d'erreur est affiché.
+ * 
+ * @param {number} itemId - L'identifiant de l'article à supprimer.
+ * @param {number} userId - L'identifiant de l'utilisateur qui supprime l'article.
+ */
 function supprimerItemPanier(itemId, userId) {
     if (!confirm("Voulez-vous vraiment supprimer cet article du panier ?")) return;
 
@@ -69,3 +92,15 @@ function supprimerItemPanier(itemId, userId) {
     })
     .catch(error => console.error("Erreur:", error));
 }
+
+//Met à jour le prix total du panier en appelant updateTotal() lors du chargement de la page(après le DOM)
+document.addEventListener('DOMContentLoaded', () => {
+    updateTotal();
+
+    document.querySelectorAll('.boutonquanti').forEach(button => {
+        button.addEventListener('click', () => {
+            setTimeout(updateTotal, 50); 
+        });
+    });
+});
+
